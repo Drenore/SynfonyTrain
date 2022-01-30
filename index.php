2537df42d12236e18b19e54f5b800113c9ca7c42
Drenore <benjamin.cristinelli1@gmail.com>
2022-01-30 16:34:14 +0100

    Ajout d'autoload composer
    ajout classe

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
 
-spl_autoload_register(function($className){
+require "vendor/autoload.php";
 
-   
-    $className = str_replace("App", "Class", $className);
-    $className = str_replace("\\", "/", $className);
-    var_dump($className); die();
-
-});
-
-// require 'class/Calcul.php'; 
 
 $instance = new App\Calcul();
 
