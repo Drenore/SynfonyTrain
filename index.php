ae48adbb897e2fdce781ba2f57554ce142abb6c6
Drenore <benjamin.cristinelli1@gmail.com>
2022-01-30 17:03:49 +0100

    Ajout Packagist
    Ajout d'un Packagist grâce à composer

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 
 require "vendor/autoload.php";
 
-
 $instance = new App\Calcul();
 
 $resultat = $instance->additionner(10,20);
 var_dump($resultat);
 
+use Carbon\Carbon;
 
-echo "Hello, World !" ;
+$aujourdhui = Carbon::now();
+
+echo $aujourdhui->format('d-m-Y');
+
+echo " <br> Hello, World !" ;
